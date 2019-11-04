934592264423a0b2cfc87f9f96c272d46b189c93
Miron Banks <madapegarage@gmail.com>
2019-11-04 20:05:00 +0100

    reset function

--- app.js
+++ app.js
@@ -13,21 +13,13 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 /*function to create random number from 0 til 6 +1
 dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
 */
 
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 // BTN ROLL FUNCTION
 
@@ -87,4 +79,28 @@ function nextPlayer (){
 
   //hiding dice after hiting 1
   document.querySelector('.dice').style.display = 'none';
+}
+
+// BTN NEW GAME FUNCTION
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// reset function
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
 }
